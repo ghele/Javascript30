c8a31dc0d3c43ecca9b6e640a2231dc0225bdc8a
ghele <gheletus_raoul@yahoo.com>
2017-01-06 23:07:12 +0200

    Added the full screen functionality to the Custom HTML5 Video Player entitled lesson

--- CustomHTML5VideoPlayer/video.js
+++ CustomHTML5VideoPlayer/video.js
@@ -3,9 +3,10 @@ const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
-const toggle = player.querySelector('.toggle');
+const playerButton = player.querySelector('.player__button');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const fullscreenButton = player.querySelector('.fullscreen__button');
 
 /* Build out functions */
 function togglePlay() {
@@ -20,7 +21,7 @@ function togglePlay() {
 
 function updateButton() {
   const icon = this.paused ? '►' : '❚ ❚';
-  toggle.textContent = icon;
+  playerButton.textContent = icon;
 }
 
 function skip() {
@@ -28,8 +29,6 @@ function skip() {
 }
 
 function handleRangeUpdate() {
-  console.log(this.name);
-  console.log(this.value);
   video[this.name] = this.value;
 }
 
@@ -39,8 +38,44 @@ function handleProgress() {
 }
 
 function scrub(e) {
+  console.log(e);
   const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
   video.currentTime = scrubTime;
+}
+
+let isNotFullScreen = (document.fullscreenElement === null)       ||
+                      (document.msFullscreenElement === null)     ||
+                      (document.webkitFullscreenElement === null) ||
+                      (document.mozFullScreenElement === null);
+
+function toggleFullScreen() {
+  if (isNotFullScreen) {
+    launchIntoFullscreen();
+  } else {
+    exitFullscreen();
+  }
+}
+
+function launchIntoFullscreen() {
+  if(video.requestFullscreen) {
+    video.requestFullscreen();
+  } else if(video.mozRequestFullScreen) {
+    video.mozRequestFullScreen();
+  } else if(video.webkitRequestFullscreen) {
+    video.webkitRequestFullscreen();
+  } else if(video.msRequestFullscreen) {
+    video.msRequestFullscreen();
+  }
+}
+
+function exitFullscreen() {
+  if(document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if(document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if(document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
 }
 
 /* Hook up the event listeners */
@@ -49,7 +84,7 @@ video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 video.addEventListener('timeupdate', handleProgress);
 
-toggle.addEventListener('click', togglePlay);
+playerButton.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip))
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 // ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
@@ -59,3 +94,5 @@ progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e) );
 progress.addEventListener('mousemove', () => mousedown = true);
 progress.addEventListener('mousemove', () => mousedown = false);
+
+fullscreenButton.addEventListener('click', toggleFullScreen);
