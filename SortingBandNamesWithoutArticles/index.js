dc9b16c587bf90006e750d20e3a0ca2ed21f28f4
ghele <gheletus_raoul@yahoo.com>
2017-01-13 14:50:52 +0200

    Implemented the Sorting Band Names Without Articles entitled lesson - Wes Bos' solution

--- SortingBandNamesWithoutArticles/index.js
+++ SortingBandNamesWithoutArticles/index.js
@@ -1,42 +1,55 @@
-const bandsForm = document.querySelector("#bands");
+// Raoul's implementation
+// const bandsForm = document.querySelector("#bands");
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean',
                'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts',
                'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-function checkWord(bandName) {
-  const wordsLength = bandName.length;
-  const bandWords = bandName.split(' ');
+// function checkWord(bandName) {
+//   const wordsLength = bandName.length;
+//   const bandWords = bandName.split(' ');
+//
+//   if (bandWords[0] === 'The' || bandWords[0] === 'A' || bandWords[0] === 'An') {
+//     return {
+//             article: bandWords[0],
+//             afterArticle: bandWords.slice(1, wordsLength).join(' ')
+//            };
+//
+//   } else {
+//     return {
+//             article: '',
+//             afterArticle: bandWords.join(' ')
+//            }
+//   }
+// }
+//
+// const allBands = [];
+//
+// bands.map((band) => {
+//   allBands.push(checkWord(band));
+// });
+//
+// const sortedBands = allBands.sort((a, b) => a.afterArticle > b.afterArticle ? 1 : -1);
+//
+// bandsForm.innerHTML =  sortedBands.map((name) => {
+//   if (name.article.length === 0) {
+//     return `
+//       <li>${name.afterArticle}</li>
+//     `;
+//   } else {
+//     return `
+//       <li>${name.article} ${name.afterArticle}</li>
+//     `;
+//   }
+// }).join('');
 
-  if (bandWords[0] === 'The' || bandWords[0] === 'A' || bandWords[0] === 'An') {
-    return {
-            article: bandWords[0],
-            afterArticle: bandWords.slice(1, wordsLength).join(' ')
-           };
-
-  } else {
-    return {
-            article: '',
-            afterArticle: bandWords.join(' ')
-           }
-  }
+// Wes Bos' implementation
+function strip(bandName) {
+  return bandName.replace(/^(a |the |an )/i, '').trim();
 }
 
-const allBands = [];
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1 );
 
-bands.map((band) => {
-  allBands.push(checkWord(band));
-});
-
-const sortedBands = allBands.sort((a, b) => a.afterArticle > b.afterArticle ? 1 : -1);
-
-bandsForm.innerHTML = sortedBands.map((name) => {
-  if (name.article.length === 0) {
-    return `
-      <li>${name.afterArticle}</li>
-    `;
-  } else {
-    return `
-      <li>${name.article} ${name.afterArticle}</li>
-    `;
-  }
-}).join('');
+document.querySelector('#bands').innerHTML =
+  sortedBands
+    .map(band => `<li>${band}</li>`)
+    .join('');
