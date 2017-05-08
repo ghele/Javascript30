2096f16c38b3d1f1798b8f8481f3e94d04ccb398
ghele <gheletus_raoul@yahoo.com>
2017-05-09 00:20:32 +0300

    Finished the Local Storage and Event Delegation entitled lesson

--- LocalStorageAndEventDelegation/index.js
+++ LocalStorageAndEventDelegation/index.js
@@ -1,6 +1,6 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = [];
+const items = JSON.parse(localStorage.getItem('items')) || [];
 
 function addItem(e) {
   e.preventDefault();
@@ -12,7 +12,8 @@ function addItem(e) {
   };
 
   items.push(item);
-  populateList(items, itemsList)
+  populateList(items, itemsList);
+  localStorage.setItem('items', JSON.stringify(items));
   this.reset();
 }
 
@@ -27,4 +28,16 @@ function populateList(plates = [], plateList) {
   }).join('');
 }
 
+function toggleDone(e) {
+  if (!e.target.matches('input')) return;  // skip this unless it's an input
+  const el = e.target;
+  const index = el.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItem('items', JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
 addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
+
+populateList(items, itemsList);
